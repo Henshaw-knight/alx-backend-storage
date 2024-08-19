6bba5cc58a613d2812ba7685913eaefe5bc73855
Henshaw <josephhensh2000@gmail.com>
2024-08-19 08:50:45 -0700

    Update file

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,37 +4,33 @@ MongoDB in the required way"""
 from pymongo import MongoClient
 
 
-"""Connect to the MongoDB server"""
-client = MongoClient(host="localhost", port=27017)
-db = client.logs
-collection = db.nginx
+def count_all_necessary_docs_by_query(collection):
+    """Counts and prints out the necessary result after the query"""
+    total_log_count = collection.count_documents({})
+    get_method_count = collection.count_documents({"method": "GET"})
+    post_method_count = collection.count_documents({"method": "POST"})
+    put_method_count = collection.count_documents({"method": "PUT"})
+    patch_method_count = collection.count_documents({"method": "PATCH"})
+    delete_method_count = collection.count_documents({"method": "DELETE"})
+    status_path_count = collection.count_documents({"path": "/status"})
 
-"""Count all documents in the collection"""
-total_log_count = collection.count_documents({})
+    print(f"{total_log_count} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get_method_count}")
+    print(f"\tmethod POST: {post_method_count}")
+    print(f"\tmethod PUT: {put_method_count}")
+    print(f"\tmethod PATCH: {patch_method_count}")
+    print(f"\tmethod DELETE: {delete_method_count}")
+    print(f"{status_path_count} status check")
 
-"""Count all documents in the collection with a GET method"""
-get_method_count = collection.count_documents({"method": "GET"})
 
-"""Count all documents in the collection with a POST method"""
-post_method_count = collection.count_documents({"method": "POST"})
+def run():
+    """Connects to the MongoDB server and runs the above function"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+    count_all_necessary_docs_by_query(nginx_collection)
 
-"""Count all documents in the collection with PUT method"""
-put_method_count = collection.count_documents({"method": "PUT"})
 
-"""Count all documents in the collection with PATCH method"""
-patch_method_count = collection.count_documents({"method": "PATCH"})
-
-"""Count all documents in the collection with DELETE method"""
-delete_method_count = collection.count_documents({"method": "DELETE"})
-
-"""Count all documents in the collection with status path"""
-status_path_count = collection.count_documents({"path": "/status"})
-
-print(f"{total_log_count} logs")
-print("Methods:")
-print(f"\tmethod GET: {get_method_count}")
-print(f"\tmethod POST: {post_method_count}")
-print(f"\tmethod PUT: {put_method_count}")
-print(f"\tmethod PATCH: {patch_method_count}")
-print(f"\tmethod DELETE: {delete_method_count}")
-print(f"{status_path_count} status check")
+if __name__ == "__main__":
+    run()
