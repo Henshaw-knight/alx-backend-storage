6e565cb50d548b2a6beeba2eb80fa4cf881c9c85
Henshaw <josephhensh2000@gmail.com>
2024-08-19 08:07:02 -0700

    Update file

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,23 +4,30 @@ MongoDB in the required way"""
 from pymongo import MongoClient
 
 
+"""Connect to the MongoDB server"""
 client = MongoClient(host="localhost", port=27017)
 db = client.logs
 collection = db.nginx
 
-total_log_count = collection.count_documents({})
-get_method_count = collection.count_documents({"method": "GET"})
-post_method_count = collection.count_documents({"method": "POST"})
-put_method_count = collection.count_documents({"method": "PUT"})
-patch_method_count = collection.count_documents({"method": "PATCH"})
-delete_method_count = collection.count_documents({"method": "DELETE"})
-status_path_count = collection.count_documents({"path": "/status"})
 
-print(f"{total_log_count} logs")
-print("Methods:")
-print(f"\tmethod GET: {get_method_count}")
-print(f"\tmethod POST: {post_method_count}")
-print(f"\tmethod PUT: {put_method_count}")
-print(f"\tmethod PATCH: {patch_method_count}")
-print(f"\tmethod DELETE: {delete_method_count}")
-print(f"{status_path_count} status check")
+def count_and_print_stats():
+    """Counts and prints the necessary stats"""
+    total_log_count = collection.count_documents({})
+    get_method_count = collection.count_documents({"method": "GET"})
+    post_method_count = collection.count_documents({"method": "POST"})
+    put_method_count = collection.count_documents({"method": "PUT"})
+    patch_method_count = collection.count_documents({"method": "PATCH"})
+    delete_method_count = collection.count_documents({"method": "DELETE"})
+    status_path_count = collection.count_documents({"path": "/status"})
+
+    print(f"{total_log_count} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get_method_count}")
+    print(f"\tmethod POST: {post_method_count}")
+    print(f"\tmethod PUT: {put_method_count}")
+    print(f"\tmethod PATCH: {patch_method_count}")
+    print(f"\tmethod DELETE: {delete_method_count}")
+    print(f"{status_path_count} status check")
+
+
+count_and_print_stats()
