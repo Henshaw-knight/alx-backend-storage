0d44bb2cb9d3f99efc3fadbd83b45b88ce199ca1
Henshaw <josephhensh2000@gmail.com>
2024-08-21 16:16:43 +0000

    Update file: add custome get method to convert data back to desired format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,3 +20,26 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key, fn=None):
+        """Takes in a `key` string argument and an optional Callable args `fn`.
+        The callable will be used to convert the data back to the desired
+        format. If key does not exist, the original Redis.get method is used
+        """
+        value = self._redis.get(key)
+        if (value is not None) and (fn is not None):
+            try:
+                int_repr = int(value.decode("utf-8"))
+                return fn(int_repr)
+            except ValueError:
+                return fn(value)
+        else:
+            return value
+
+    def get_str(self, string_value):
+        """Returns the converted string data value format"""
+        return string_value
+
+    def get_int(self, int_value):
+        """Returns the converted integer data value format"""
+        return int_value
